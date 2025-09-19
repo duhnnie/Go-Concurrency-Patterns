92106af59c404ae4515654f804134eb1c0e6072b
Daniel Canedo <dnl.canedo@gmail.com>
2025-09-19 16:14:02 -0400

    Pub/Sub: add challenge #1

--- 04-pub-sub/challenges/01-topic-based-pub-sub/solution.go
+++ 04-pub-sub/challenges/01-topic-based-pub-sub/solution.go
@@ -8,63 +8,62 @@ import (
 
 type Broker struct {
 	mu               sync.RWMutex
-	topicSubscribers map[string][]chan<- string
-	allSubscribers   []chan<- string
+	topicSubscribers map[string][]chan string
+	allSubscribers   []chan string
 }
 
 func NewBroker() *Broker {
 	return &Broker{
-		topicSubscribers: make(map[string][]chan<- string),
-		allSubscribers:   []chan<- string{},
+		topicSubscribers: make(map[string][]chan string),
+		allSubscribers:   []chan string{},
 	}
 }
 
-func (b *Broker) Subscribe(topic string) <-chan string {
-	subscriberChan := make(chan string)
+func (b *Broker) Subscribe(topic string, bufferSize int) <-chan string {
+	ch := make(chan string, bufferSize)
 
-	b.mu.Lock() // Lock access to subscribers maps for cases of concurrent access
+	b.mu.Lock() // Lock access to subscribers map while we update it
 	defer b.mu.Unlock()
 
 	if topic == "all" {
-		b.allSubscribers = append(b.allSubscribers, subscriberChan)
+		b.allSubscribers = append(b.allSubscribers, ch)
 	} else {
-		b.topicSubscribers[topic] = append(b.topicSubscribers[topic], subscriberChan)
+		b.topicSubscribers[topic] = append(b.topicSubscribers[topic], ch)
 	}
 
-	return subscriberChan
+	return ch
 }
 
 func (b *Broker) Publish(topic, message string) {
-	b.mu.RLock() // Lock write access while reading all subscribers for deliver messages
+	b.mu.RLock() // Lock write access to subscribers map while we reading it
 	defer b.mu.RUnlock()
 
-	if subscribers, ok := b.topicSubscribers[topic]; ok {
-		for _, subscriber := range subscribers {
+	if subs, ok := b.topicSubscribers[topic]; ok {
+		for _, sub := range subs {
+			// Use select/case to drop messages instead of blocking indefinitely (a common Pub/Sub approach).
 			select {
-			case subscriber <- message:
+			case sub <- message:
 			default:
-				// Drops message if subscriber channel is full (avoid blocking)
-				fmt.Println("Warning: dropped message for topic", topic)
+				// Drop message if subscriber channel is full (avoid blocking)
+				fmt.Printf("Warning: dropped message on topic %q (subscriber too slow)\n", topic)
 			}
-
 		}
 	}
 
-	for _, allSubscriber := range b.allSubscribers {
+	for _, sub := range b.allSubscribers {
 		select {
-		case allSubscriber <- message:
+		case sub <- message:
 		default:
-			fmt.Println("Warning: dropped message for subscribers to \"all\"")
+			fmt.Println("Warning: dropped message for \"all\"", message)
 		}
-
 	}
 }
 
 func main() {
 	broker := NewBroker()
-	sportsSub := broker.Subscribe("sports")
-	_ = broker.Subscribe("news")
-	logger := broker.Subscribe("all")
+	sportsSub := broker.Subscribe("sports", 5)
+	newsSub := broker.Subscribe("news", 5)
+	logger := broker.Subscribe("all", 5)
 
 	go func() {
 		for message := range sportsSub {
@@ -72,11 +71,11 @@ func main() {
 		}
 	}()
 
-	// go func() {
-	// 	for message := range newsSub {
-	// 		fmt.Printf("News channel: %s\n", message)
-	// 	}
-	// }()
+	go func() {
+		for message := range newsSub {
+			fmt.Printf("News channel: %s\n", message)
+		}
+	}()
 
 	go func() {
 		for message := range logger {
@@ -86,7 +85,8 @@ func main() {
 
 	broker.Publish("sports", "Team A won the match!")
 	broker.Publish("news", "Breaking: Go 2.0 announced!")
-	// broker.Publish("news", "Evo Morales goes to jail!")
+	broker.Publish("sports", "Bolivia classifies to FIFA World Cup 2026!")
+	broker.Publish("news", "Asteroid is coming to Earth!")
 
-	time.Sleep(10 * time.Second)
+	time.Sleep(2 * time.Second)
 }
